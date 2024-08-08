b90a6d771a4ce510542abd69f7dcb616245b3c81
NamLe0609 <lehoangnamtep@gmail.com>
2024-08-08 23:26:48 +0200

    Convert equation into function

--- relativistic-boris.cpp
+++ relativistic-boris.cpp
@@ -23,6 +23,41 @@ struct Field {
     float z;
 };
 
+void relativisticBoris(Particle &electron, const Field &eField, const Field &bField, float timestep) {
+    // Half-step momentum from Electric field
+    electron.px += timestep * eField.x / 2.0f;
+    electron.py += timestep * eField.y / 2.0f;
+    electron.pz += timestep * eField.z / 2.0f;
+
+    // Lorentz factor for half-step momentum
+    float lorentz = std::sqrt(1.0f + electron.px*electron.px + electron.py*electron.py + electron.pz*electron.pz);
+
+    // Rotation vector from Magnetic field
+    float tx = timestep * bField.x / (2.0f * lorentz);
+    float ty = timestep * bField.y / (2.0f * lorentz);
+    float tz = timestep * bField.z / (2.0f * lorentz);
+    float tMagSquare = tx*tx + ty*ty + tz*tz;
+
+    // Cross product of half p and t
+    float pxPrime = electron.px + (electron.py * tz - electron.pz * ty); 
+    float pyPrime = electron.py + (electron.pz * tx - electron.px * tz); 
+    float pzPrime = electron.pz + (electron.px * ty - electron.py * tx); 
+
+    // Update momentum with effect from Boris rotation and Electric field 
+    float denominator = 1 + tMagSquare;
+    electron.px += 2 * (pyPrime * tz - pzPrime * ty) / denominator + timestep * eField.x / 2;
+    electron.py += 2 * (pzPrime * tx - pxPrime * tz) / denominator + timestep * eField.y / 2;
+    electron.pz += 2 * (pxPrime * ty - pyPrime * tx) / denominator + timestep * eField.z / 2;
+ 
+    // Lorentz factor for updated momentum
+    lorentz = std::sqrt(1.0f + electron.px*electron.px + electron.py*electron.py + electron.pz*electron.pz);
+
+    // Update position using calculated velocity
+    electron.x += timestep * electron.px / lorentz;
+    electron.y += timestep * electron.py / lorentz;
+    electron.z += timestep * electron.pz / lorentz;
+}
+
 int main() {
     // Choose arbitrary timesteps
     float timestep = 0.025f; 
@@ -33,40 +68,9 @@ int main() {
 
     // Initialize particle with fixed data
     Particle test = {0.25f, 0.25f, 0.25f, 10.0f, 10.0f, 10.0f};
-
-    // Half-step momentum from Electric field
-    test.px += timestep * eField.x / 2.0f;
-    test.py += timestep * eField.y / 2.0f;
-    test.pz += timestep * eField.z / 2.0f;
-
-    // Lorentz factor for half-step momentum
-    float lorentz = std::sqrt(1.0f + test.px*test.px + test.py*test.py + test.pz*test.pz);
-
-    // Rotation vector from Magnetic field
-    float tx = timestep * bField.x / (2.0f * lorentz);
-    float ty = timestep * bField.y / (2.0f * lorentz);
-    float tz = timestep * bField.z / (2.0f * lorentz);
-    float tMagSquare = tx*tx + ty*ty + tz*tz;
-
-    // Cross product of half p and t
-    float pxPrime = test.px + (test.py * tz - test.pz * ty); 
-    float pyPrime = test.py + (test.pz * tx - test.px * tz); 
-    float pzPrime = test.pz + (test.px * ty - test.py * tx); 
-
-    // Update momentum with effect from Boris rotation and Electric field 
-    float denominator = 1 + tMagSquare;
-    test.px += 2 * (pyPrime * tz - pzPrime * ty) / denominator + timestep * eField.x / 2;
-    test.py += 2 * (pzPrime * tx - pxPrime * tz) / denominator + timestep * eField.y / 2;
-    test.pz += 2 * (pxPrime * ty - pyPrime * tx) / denominator + timestep * eField.z / 2;
- 
-    // Lorentz factor for updated momentum
-    lorentz = std::sqrt(1.0f + test.px*test.px + test.py*test.py + test.pz*test.pz);
-
-    // Update position using calculated velocity
-    test.x += timestep * test.px / lorentz;
-    test.y += timestep * test.py / lorentz;
-    test.z += timestep * test.pz / lorentz;
-
+    
+    relativisticBoris(test, eField, bField, timestep);
+    
     // Print particle
     std::cout << test.print() << "\n";
 
